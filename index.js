b1df87a640458187008eacb55a87219b2c9e3a4c
Kha-lil <ikumapayiolaniyi@gmail.com>
2020-11-01 01:49:00 +0100

    initial committ

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 "use strict";
 
-const hours = document.querySelector('#hours');
-const extraHours = document.querySelector('#extra-hours');
-const calcBtn = document.querySelector('button');
-const hoursWorkedDetail = document.querySelector('.hours-worked-detail');
-const extraHoursWorkedDetail = document.querySelector('.extra-hours-worked-detail');
-const totalSalary = document.querySelector('.total-salary');
+const hours = document.querySelector("#hours");
+const extraHours = document.querySelector("#extra-hours");
+const calcBtn = document.querySelector("button");
+const hoursWorkedDetail = document.querySelector(".hours-worked-detail");
+const extraHoursWorkedDetail = document.querySelector(
+  ".extra-hours-worked-detail"
+);
+const totalSalary = document.querySelector(".total-salary");
 
 let hoursWorked;
 let extraHoursWorked;
 let salary;
 
-function calculateSalary (hours, extraHours) {
-  hoursWorked = +hours.value  * 1000;
+function calculateSalary(hours, extraHours) {
+  hoursWorked = +hours.value * 1000;
   extraHoursWorked = +extraHours.value * 200;
   if (hoursWorked !== 0) {
-    if (hoursWorked < 0 || extraHoursWorked < 0 ) {
-      alert('Input a positive number');
+    if (hoursWorked < 0 || extraHoursWorked < 0) {
+      alert("Input a positive number");
     } else {
       salary = hoursWorked + extraHoursWorked;
       showSalary();
     }
   } else {
-    alert('Please, input the total number of employee working hour.');
+    alert("Please, input the total number of employee working hour.");
   }
 
   function showSalary() {
@@ -37,6 +39,6 @@ function showEmployeeTWH() {
   totalSalary.innerHTML = `<p>Your Total Salary is #${salary}</p>`;
 }
 
-calcBtn.addEventListener('click', function () {
+calcBtn.addEventListener("click", function () {
   calculateSalary(hours, extraHours);
-})
+});
